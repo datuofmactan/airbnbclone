c0b850774862f871aff9af21b57d6728ac2831ec
Rodrigo Masangya <rodrigomasangya@gmail.com>
2024-05-27 08:14:19 -0700

    all changes

---
+++ scrimba/vite-react/airbnb_clone/src/index.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+// import Navbar from "./components/Navbar" 
+// import Main from "./components/Main"
+import App from "./App"
+
+ReactDOM.render(<App />, document.getElementById("root"))
